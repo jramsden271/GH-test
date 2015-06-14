6736d2c79351c537ba40dc5f4f454e91f78a6e39
jramsden271 <jramsden271@gmail.com>
2015-06-14 19:42:07 +0100

    First commit
    First commit

---
+++ GH test/GH testInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace GH_test
+{
+  public class GH testInfo : GH_AssemblyInfo
+  {
+    public override string Name
+    {
+      get
+      {
+        return "GHtest";
+      }
+    }
+    public override Bitmap Icon
+    {
+      get
+      {
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        return null;
+      }
+    }
+    public override string Description
+    {
+      get
+      {
+        //Return a short string describing the purpose of this GHA library.
+        return "";
+      }
+    }
+    public override Guid Id
+    {
+      get
+      {
+        return new Guid("9b0fe810-ca3d-4ed1-b38f-b9a9af8e8393");
+      }
+    }
+
+    public override string AuthorName
+    {
+      get
+      {
+        //Return a string identifying you or your company.
+        return "";
+      }
+    }
+    public override string AuthorContact
+    {
+      get
+      {
+        //Return a string representing your preferred contact details.
+        return "";
+      }
+    }
+  }
+}
